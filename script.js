c7d9bd892dbb035125bb16f7dfd126f2cd684df5
jonathanla-25 <davidlavayen14@hotmail.com>
2026-04-01 15:40:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-let tiempo = 3600;
+window.addEventListener('scroll', () => {
 
-const contador = document.getElementById("contador");
+    const scroll = window.scrollY;
 
-setInterval(() => {
-    let h = Math.floor(tiempo / 3600);
-    let m = Math.floor((tiempo % 3600) / 60);
-    let s = tiempo % 60;
+    // CAMBIO DE OPACIDAD DEL HERO
+    const overlay = document.querySelector('.overlay');
+    overlay.style.backdropFilter = `blur(${6 + scroll * 0.02}px)`;
 
-    contador.innerHTML = `${h}:${m}:${s < 10 ? '0' : ''}${s}`;
+    // ANIMACIONES
+    document.querySelectorAll('.reveal').forEach(el => {
+        let top = el.getBoundingClientRect().top;
+        if (top < window.innerHeight - 50) {
+            el.classList.add('active');
+        }
+    });
 
-    if (tiempo > 0) tiempo--;
-}, 1000);
+});
