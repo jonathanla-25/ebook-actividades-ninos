02a1542cde7e532f200b8c7445c0980610249056
jonathanla-25 <davidlavayen14@hotmail.com>
2026-04-01 15:51:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 window.addEventListener('scroll', () => {
-
-    const scroll = window.scrollY;
-
-    // CAMBIO DE OPACIDAD DEL HERO
-    const overlay = document.querySelector('.overlay');
-    overlay.style.backdropFilter = `blur(${6 + scroll * 0.02}px)`;
-
-    // ANIMACIONES
     document.querySelectorAll('.reveal').forEach(el => {
         let top = el.getBoundingClientRect().top;
         if (top < window.innerHeight - 50) {
             el.classList.add('active');
         }
     });
-
 });
