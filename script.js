99b9ad08d77c843b055d9d2009829093d3faaf9b
jonathanla-25 <davidlavayen14@hotmail.com>
2026-04-01 14:19:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-// CONTADOR
 let tiempo = 3600;
+
 const contador = document.getElementById("contador");
 
 setInterval(() => {
-    let m = Math.floor(tiempo / 60);
+    let h = Math.floor(tiempo / 3600);
+    let m = Math.floor((tiempo % 3600) / 60);
     let s = tiempo % 60;
 
-    contador.innerHTML = `⏳ Oferta termina en: ${m}:${s < 10 ? '0' : ''}${s}`;
+    contador.innerHTML = `${h}:${m}:${s < 10 ? '0' : ''}${s}`;
+
     if (tiempo > 0) tiempo--;
 }, 1000);
-
-// SCROLL ANIMATION
-window.addEventListener('scroll', () => {
-    document.querySelectorAll('.reveal').forEach(el => {
-        let top = el.getBoundingClientRect().top;
-        if (top < window.innerHeight - 50) {
-            el.classList.add('active');
-        }
-    });
-});
